6c5b8664369ba52f4e94f27742b2ae432f2ea5d8
Yuri Isbener <yurisuhett@gmail.com>
2020-11-24 22:02:41 -0300

    trab feito

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicios_de_fixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[15];
+            for (var i = 0; i < 15; i++)
+            {
+                Console.Write("Insira um valor: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Reverse(numeros);
+            foreach (int item in numeros)
+            {
+            Console.WriteLine(item);
+            }
+        }
+    }
+}
